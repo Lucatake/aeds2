00822605bec554b5249a2ec907f06316576d5b9c
Luana Catake <takeishi.lc@gmail.com>
2021-03-13 13:49:54 +0000

    a

--- tps/entrada e saida/tp01/TP01Q01 - Palíndromo em Java/Palindromo.java
+++ tps/entrada e saida/tp01/TP01Q01 - Palíndromo em Java/Palindromo.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Palindromo {
 
    public static boolean isFim(String s){
@@ -33,3 +34,40 @@ class Palindromo {
       }
    }
 }
+=======
+class Palindromo {
+
+   public static boolean isFim(String s){
+      return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+   }
+
+  
+   public static String isPalindromo (String s){
+      String invert = new StringBuffer(s).reverse().toString();
+      String isP = "NAO";
+
+      if(s.equals(invert))
+      {
+        isP = "SIM";
+      }
+
+      return isP;
+   }
+
+   public static void main (String[] args){
+      String[] entrada = new String[1000];
+      int numEntrada = 0;
+
+      //Leitura da entrada padrao
+      do {
+         entrada[numEntrada] = MyIO.readLine();
+      } while (isFim(entrada[numEntrada++]) == false);
+      numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+
+      //Para cada linha de entrada, gerando uma de saida boleana se e' ou nao palindromo
+      for(int i = 0; i < numEntrada; i++){
+         MyIO.println(isPalindromo(entrada[i]));
+      }
+   }
+}
+>>>>>>> origin/master
