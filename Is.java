49a2859e5912e04cd1de665bc3e4e59870ba95a5
Luana Catake <takeishi.lc@gmail.com>
2021-03-13 19:09:52 +0000

    tentando Q6

---
+++ Is.java
@@ -0,0 +1,156 @@
+class Is {
+
+   public static boolean isFim(String s){
+      return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+   }
+
+   public static boolean allVogal (String s){
+     boolean is = true;
+     boolean vog = false;
+     String vogais = new String();
+     vogais = "aeiouAEIOUÁÉÍÓÚáéíóúÀÈÌÒÙàèìòùãõÃÕÂÊÎÔÛâêîôû";
+
+     for (int i = 0; i < s.length(); i++) {
+       int j = 0;
+
+       do{
+         if(is == true && !vog && s.charAt(i) == vogais.charAt(j)){
+           vog = true;
+           is = true;
+         } else{
+           is = false;
+         }
+
+         j++;
+       } while(j<44 && !vog && is == true);
+     }
+
+     return is;
+  }
+
+   public static boolean allConsonant (String s){
+     boolean is = true;
+     boolean vog = false;
+     String vogais = new String();
+     vogais = "aeiouAEIOUÁÉÍÓÚáéíóúÀÈÌÒÙàèìòùãõÃÕÂÊÎÔÛâêîôû";
+
+     for (int i = 0; i < s.length(); i++) {
+       int j = 0;
+
+       do{
+         if(!vog && s.charAt(i) == vogais.charAt(j)){
+           vog = true;
+           is = false;
+         } else if(vog != true){
+           is = true;
+         }
+
+         j++;
+       } while(j<44 && !vog && is == true);
+     }
+
+     return is;
+  }
+
+   public static boolean allInt (String s){
+     boolean is = true;
+     boolean nums = false;
+     String num = new String();
+     num = "0123456789";
+
+     for (int i = 0; i < s.length(); i++) {
+       int j = 0;
+
+       do{
+         if(is == true && !nums && s.charAt(i) == num.charAt(j)){
+           nums = true;
+           is = true;
+         } else{
+           is = false;
+         }
+
+         j++;
+       } while(j<10 && !nums && is == true);
+       nums = false;
+     }
+
+     return is;
+  }
+
+   public static boolean allReal (String s){
+     boolean is = true;
+     boolean nums = false;
+     boolean point = false;
+     String num = new String();
+     num = "0123456789";
+
+     for (int i = 0; i < s.length(); i++) {
+       int j = 0;
+
+       do{
+         if(is == true && !nums && s.charAt(i) == num.charAt(j)){
+           nums = true;
+           is = true;
+         } else if(s.charAt(i) =='.' && !point){
+           point = true;
+           is = true;
+         } else{
+           is = false;
+         }
+
+         j++;
+       } while(j<10 && !nums && is == true);
+       nums = false;
+     }
+
+     return is;
+  }
+
+   public static String isAll (String s){
+      String result = new String();
+      result = "";
+
+      if(allVogal(s)){
+        result = result + "SIM ";
+      } else {
+        result = result + "NAO ";
+      }
+
+      if(allConsonant(s)){
+        result = result + "SIM ";
+      } else {
+        result = result + "NAO ";
+      }
+
+      if(allInt(s)){
+        result = result + "SIM ";
+      } else {
+        result = result + "NAO ";
+      }
+
+      if(allReal(s)){
+        result = result + "SIM";
+      } else {
+        result = result + "NAO";
+      }
+
+      return result;
+   }
+
+   public static void main (String[] args){
+      String[] entrada = new String[1000];
+      int numEntrada = 0;
+
+      //Leitura da entrada padrao
+      do {
+         entrada[numEntrada] = MyIO.readLine();
+      } while (isFim(entrada[numEntrada++]) == false);
+      numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+
+      //Para cada linha de entrada, gerando uma de saida SIM e NAO dos resultados
+      //X1 - vogais | X2 - consoantes | X3 - numero int | X4 - numero real
+      for(int i = 0; i < numEntrada; i++){
+         MyIO.println(isAll(entrada[i]));
+      }
+   }
+}
---
+++ tps/entrada e saida/tp01/TP01Q05 - Álgebra Booleana em Java/Algebra.java
@@ -0,0 +1,47 @@
+class Algebra {
+
+   public static boolean isFim(String s){
+      return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+   }
+
+   public static boolean algebraBool (String s){
+      boolean resp = false;
+      int numVariaveis = (int)s.charAt(0);
+      int v[] = new int[numVariaveis];
+      int j = 0;
+      String[] eq = new String[1000];
+
+      for(int i = 2; i <= numVariaveis*2; i+=2){
+        v[j] = (int)s.charAt(i);
+        j++;
+      }
+
+      for(int i = 0; i < s.length(); i++){
+        if(s.charAt(i) == "n" && s.charAt(i+1) == "o" && s.charAt(i+2) == "t"){
+
+        } else if(s.charAt(i) == "a" && s.charAt(i+1) == "n" && s.charAt(i+2) == "d"){
+
+        } else if(s.charAt(i) == "o" && s.charAt(i+1) == "r"){
+          
+        }
+      }
+
+      return resp;
+   }
+
+   public static void main (String[] args){
+      String[] entrada = new String[1000];
+      int numEntrada = 0;
+
+      //Leitura da entrada padrao
+      do {
+         entrada[numEntrada] = MyIO.readLine();
+      } while (isFim(entrada[numEntrada++]) == false);
+      numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+
+      //Para cada linha de entrada, gerando uma de saida boleana resultado da algebra.
+      for(int i = 0; i < numEntrada; i++){
+         MyIO.println(algebraBool(entrada[i]));
+      }
+   }
+}
