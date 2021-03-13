e242a3bb1ba09e009261539d8cc9893b421d2f44
Luana Catake <takeishi.lc@gmail.com>
2021-03-13 13:47:04 +0000

    pronto

---
+++ tps/entrada e saida/tp01/TP01Q04 - Alteração Aleatória em Java/Aleatorio2.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+class Aleatorio {
+
+   public static boolean isFim(String s){
+      return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+   }
+
+  
+   public static String alteracao(String texto, char a, char b) {
+      int i = texto.length();
+      String novoTexto = texto;
+
+      novoTexto = novoTexto.replace(a, b);
+ 
+      return novoTexto;
+   }
+        
+
+   public static void main (String[] args){
+      String[] entrada = new String[1000];
+      int numEntrada = 0;
+      Random gerador = new Random();
+      gerador.setSeed(4);
+
+      //Leitura da entrada padrao
+      do {
+         entrada[numEntrada] = MyIO.readLine();
+      } while (isFim(entrada[numEntrada++]) == false);
+      numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
+
+      //Para cada linha de entrada, gerando a saida com a alteracao aleatoria.
+      for(int i = 0; i < numEntrada; i++){
+         char a = (char)( 'a' + (Math.abs(gerador.nextInt()) % 26));
+         char b = (char)( 'a' + (Math.abs(gerador.nextInt()) % 26));
+         MyIO.println(alteracao(entrada[i], a, b));
+      }
+   }
+}
