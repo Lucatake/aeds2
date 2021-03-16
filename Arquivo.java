7502713c2afd3d97b0c9a7566fd20f4696079499
Luana Catake <takeishi.lc@gmail.com>
2021-03-16 15:00:53 +0000

    q9

---
+++ Arquivo.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+class Arquivo {
+    public static void main (String[] args) throws Exception{
+      double num = 0.0;
+      int i;
+      RandomAccessFile arqW = new RandomAccessFile("nums.txt", "rw");
+      //Leitura da quantidade de valores
+      int numEntrada = MyIO.readInt();
+      int aux = numEntrada;
+
+      //Leitura da entrada padrao e gravar em arquivo numeros reais
+      while(aux > 0){
+        num = MyIO.readDouble();
+        arqW.writeDouble(num);
+        aux--;
+      }
+      //Fechar arquivo, indispensavel para gravar
+      arqW.close();
+
+      RandomAccessFile arqR = new RandomAccessFile("nums.txt", "rw");
+
+      //Para cada linha gravada, ler e mostrar a saida
+      i = 640;
+      aux= 0;
+      while(arqR.read() > -1 && i > 0){
+        //Ler de arquivo
+          arqR.seek(i);
+          num = arqR.readDouble();
+          MyIO.println(num);
+          i-=8;
+          aux++;
+      }
+      //Fechar arquivo, indispensavel
+      arqR.close();
+    }
+}
