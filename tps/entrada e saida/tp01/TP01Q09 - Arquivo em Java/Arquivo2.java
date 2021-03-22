bda664526262c5a40e0dd432946e672918077106
Luana Catake <takeishi.lc@gmail.com>
2021-03-22 02:14:09 +0000

    final tp1 13 questões feitas com uma 87%

---
+++ tps/entrada e saida/tp01/TP01Q09 - Arquivo em Java/Arquivo2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class Arquivo {
+    public static void main (String[] args) throws Exception{
+      String reais;
+      double num = 0.0;
+      int i;
+      RandomAccessFile arqW = new RandomAccessFile("nums.txt", "rw");
+      //Leitura da quantidade de valores
+      int numEntrada = MyIO.readInt();
+      int aux = numEntrada;
+
+      //Leitura da entrada padrao e gravar em arquivo numeros reais
+      while(aux > 0){
+        reais = MyIO.readLine();
+        num = Double.parseDouble(reais); 
+        arqW.writeDouble(num);
+        aux--;
+      }
+      //Fechar arquivo, indispensavel para gravar
+      arqW.close();
+
+      RandomAccessFile arqR = new RandomAccessFile("nums.txt", "rw");
+
+      //Para cada linha gravada, ler e mostrar a saida
+      i = (numEntrada*8)-8;
+      aux= 0;
+      while(i >= 0){
+        //Ler de arquivo
+          arqR.seek(i);
+          num = arqR.readDouble();
+          reais = String.valueOf(num);
+          num = Double.parseDouble(reais);
+          MyIO.println(num);
+          i-=8;
+          aux++;
+      }
+      //Fechar arquivo, indispensavel
+      arqR.close();
+    }
+}
